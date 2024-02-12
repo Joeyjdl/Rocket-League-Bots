3d8175fd7e55f48c89722f90197cf5fd5093f526
Joeyjdl <joeylaarhoven@gmail.com>
2024-02-12 15:38:01 +0100

    Added tensorboard loging

--- RocketBot.py
+++ RocketBot.py
@@ -6,6 +6,11 @@ from stable_baselines3.common.callbacks import BaseCallback
 import os
 
 
+from stable_baselines3.common.monitor import Monitor
+from stable_baselines3.common.vec_env import VecNormalize
+from stable_baselines3.common.vec_env import DummyVecEnv
+
+
 from rlgym.utils.reward_functions import RewardFunction
 from rlgym.utils import math
 from rlgym.utils.gamestates import GameState, PlayerData
@@ -18,29 +23,19 @@ class SpeedReward(RewardFunction):
   def reset(self, initial_state: GameState):
     pass
 
-  def get_reward(self, player: PlayerData, state: GameState, previous_action: np.ndarray) -> float:
-    linear_velocity = player.car_data.linear_velocity
-    
-    rewards = np.empty(5)
-
-    #reward for going fast
-    rewards[0] = math.vecmag(linear_velocity) / (CAR_MAX_SPEED*2)
+  def get_reward(self, player: PlayerData, state: GameState, previous_action: np.ndarray) -> float:    
+    rewards = np.empty(3)
 
     #reward for getting close to the ball
     dist = np.linalg.norm(player.car_data.position - state.ball.position) - BALL_RADIUS
-    rewards[1] = np.exp(-0.5 * dist / CAR_MAX_SPEED)
+    rewards[0] = np.exp(-0.5 * dist / CAR_MAX_SPEED)/4
 
     #reward for speed towards the ball
     vel = player.car_data.linear_velocity
     pos_diff = state.ball.position - player.car_data.position
     norm_pos_diff = pos_diff / np.linalg.norm(pos_diff)
     vel /= CAR_MAX_SPEED
-    rewards[2] = float(np.dot(norm_pos_diff, vel))
-
-    #reward for facing the ball
-    pos_diff = state.ball.position - player.car_data.position
-    norm_pos_diff = pos_diff / np.linalg.norm(pos_diff)
-    rewards[3] = float(np.dot(player.car_data.forward(), norm_pos_diff))
+    rewards[1] = float(np.dot(norm_pos_diff, vel))
 
     #reward for getting ball to goal
     if player.team_num == BLUE_TEAM:
@@ -50,7 +45,9 @@ class SpeedReward(RewardFunction):
 
     # Compensate for moving objective to back of net
     dist = np.linalg.norm(state.ball.position - objective) - (BACK_NET_Y - BACK_WALL_Y + BALL_RADIUS)
-    rewards[4] = 2 * np.exp(-0.5 * dist / BALL_MAX_SPEED)
+    rewards[2] = (2 * np.exp(-0.5 * dist / BALL_MAX_SPEED))
+
+    # print(rewards)
     
     return np.sum(rewards)
     
@@ -58,36 +55,22 @@ class SpeedReward(RewardFunction):
     return 0
   
 
-class SaveModelCallback(BaseCallback):
-    def __init__(self, save_freq: int, save_path: str, model):
-        super(SaveModelCallback, self).__init__()
-        self.save_freq = save_freq
-        self.save_path = save_path
-        self.model = model
-
-    def _on_step(self) -> bool:
-        if self.n_calls % self.save_freq == 0:
-            PPO.save(self.model, path=self.save_path)
-        return True
-
-
 print(f"Code has begun")
 #Make the default rlgym environment
-env = rlgym.make(game_speed=100, reward_fn=SpeedReward(), auto_minimize=True)
+env = rlgym.make(game_speed=100, reward_fn=SpeedReward(), auto_minimize=False, use_injector=True)
+# env = Monitor(env, "./logs")
+# env = DummyVecEnv([lambda: env])
+# # Use VecNormalize for normalization
+# env = VecNormalize(env)
 
-#Load model or initialize PPO from stable_baselines3
-print(f"Loading has begun")
-if os.path.exists('./BotBrains/RockBotBrain.zip'):
-    model = PPO.load('./BotBrains/RockBotBrain', env=env, verbose=1)
-else:
-    model = PPO("MlpPolicy", env=env, verbose=1)
 
-#Train our agent!
-# Set up the callback to save the model every `save_freq` steps
-save_freq = 5000  # Adjust as needed
-save_path = "./BotBrains/RockBotBrain"
-save_callback = SaveModelCallback(save_freq, save_path, model)
 
-# Train the model with the callback
+
+# Train the model
 print(f"Training has begun")
-model.learn(total_timesteps=int(1e6), callback=save_callback)
+for i in range(5):
+    save_path = "./BotBrains/RockBotBrainNew" + str(i)
+    model = PPO("MlpPolicy", env=env, verbose=1, tensorboard_log="./logs")
+    tempModel = model.learn(total_timesteps=int(5e6), tb_log_name="Run_" + str(i) , progress_bar=True)
+    PPO.save(tempModel, path=save_path)
+
