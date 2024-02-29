819533ca28d38dd53daeedbfe630e8dc5ea631fb
NHClaessens <nhclaessens@gmail.com>
2024-02-29 15:46:27 +0100

    All logs

--- utils/logger/ExampleLogger.py
+++ utils/logger/ExampleLogger.py
@@ -5,31 +5,89 @@ import numpy as np
 class ExampleLogger(MetricsLogger):
     def __init__(self) -> None:
         super().__init__()
-        self.previous_goal_count = 0
+        self.track_stats = [0]*10
 
     def _collect_metrics(self, game_state: GameState) -> list:
         return [game_state.players[0].car_data.linear_velocity,
                 game_state.players[0].car_data.rotation_mtx(),
-                game_state.blue_score]
+
+                # Stats to track per episode
+                game_state.blue_score,
+                game_state.players[0].match_goals,
+                game_state.orange_score,
+                0 if len(game_state.players) < 2 else game_state.players[1].match_goals,
+                game_state.players[0].match_saves,
+                0 if len(game_state.players) < 2 else game_state.players[1].match_saves,
+                game_state.players[0].match_shots,
+                0 if len(game_state.players) < 2 else game_state.players[1].match_shots,
+                # end
+
+                # avg
+                game_state.players[0].boost_amount,
+                0 if len(game_state.players) < 2 else game_state.players[1].boost_amount,
+                game_state.players[0].car_data.position[2],
+                0 if len(game_state.players) < 2 else game_state.players[1].car_data.position[2],
+
+                # max
+                game_state.players[0].car_data.position[2],
+                0 if len(game_state.players) < 2 else game_state.players[1].car_data.position[2],
+                ]
 
     def _report_metrics(self, collected_metrics, wandb_run, cumulative_timesteps):
         avg_linvel = np.zeros(3)
+        avg_stats = [0] * 4
+        max_stats = [0] * 2
+
+
         for metric_array in collected_metrics:
             # [[velocity, rotation, score], [velocity, rotation, score], [velocity, rotation, score], [velocity, rotation, score]]
             avg_linvel += metric_array[0]
-            current_goals = metric_array[2]
+            current_stats = metric_array[2:9]
 
-            # Reset score on new game
-            if self.previous_goal_count > 0 and current_goals == 0:
-                self.previous_goal_count = 0
-            # Only count new goals
-            if current_goals > self.previous_goal_count:
-                self.previous_goal_count = current_goals
+            for (idx, item) in enumerate(current_stats):
+                if self.track_stats[idx] > 0 and item == 0:
+                    self.track_stats[idx] = 0
+                if item > self.track_stats[idx]:
+                    self.track_stats[idx]=item 
+
+            stats_to_avg = metric_array[9:13]
+            for(idx, item) in enumerate(stats_to_avg):
+                avg_stats[idx] += item
+
+            stats_to_max = metric_array[13:15]
+            for(idx, item) in enumerate(stats_to_max):
+                if item > max_stats[idx]:
+                    max_stats[idx] = item
+            
         
         avg_linvel /= len(collected_metrics)
-        report = {"x_vel":avg_linvel[0],
-                  "y_vel":avg_linvel[1],
-                  "z_vel":avg_linvel[2],
-                  "Cumulative Timesteps":cumulative_timesteps,
-                  "Goals_scored": self.previous_goal_count}
+        for(idx, item) in enumerate(avg_stats):
+                avg_stats[idx] /= len(collected_metrics)
+
+        report = {
+                    "x_vel":avg_linvel[0],
+                    "y_vel":avg_linvel[1],
+                    "z_vel":avg_linvel[2],
+                    "Cumulative Timesteps":cumulative_timesteps,
+                    "blue_team goals_scored": self.track_stats[0],
+                    "blue_player goals_scored": self.track_stats[1],
+
+                    "orange_team goals_scored": self.track_stats[2],
+                    "orange_player goals_scored": self.track_stats[3],
+
+                    "blue_player saves": self.track_stats[4],
+                    "orange_player saves": self.track_stats[5],
+
+                    "blue_player shots": self.track_stats[6],
+                    "orange_player shots": self.track_stats[7],
+
+                    "blue_player avg_boost": avg_stats[0],
+                    "orange_player avg_boost": avg_stats[1],
+
+                    "blue_player avg_height": avg_stats[2],
+                    "orange_player avg_height": avg_stats[3],
+
+                    "blue_player max_height": max_stats[0],
+                    "orange_player max_height": max_stats[1],
+                }
         wandb_run.log(report)
