6ed9e27d0c14661b36f682add5ab5e890e0dd013
NHClaessens <nhclaessens@gmail.com>
2024-02-29 12:52:53 +0100

    logger

--- utils/logger/ExampleLogger.py
+++ utils/logger/ExampleLogger.py
@@ -3,19 +3,33 @@ from rlgym_sim.utils.gamestates import GameState
 import numpy as np
 
 class ExampleLogger(MetricsLogger):
+    def __init__(self) -> None:
+        super().__init__()
+        self.previous_goal_count = 0
+
     def _collect_metrics(self, game_state: GameState) -> list:
         return [game_state.players[0].car_data.linear_velocity,
                 game_state.players[0].car_data.rotation_mtx(),
-                game_state.orange_score]
+                game_state.blue_score]
 
     def _report_metrics(self, collected_metrics, wandb_run, cumulative_timesteps):
         avg_linvel = np.zeros(3)
         for metric_array in collected_metrics:
-            p0_linear_velocity = metric_array[0]
-            avg_linvel += p0_linear_velocity
+            # [[velocity, rotation, score], [velocity, rotation, score], [velocity, rotation, score], [velocity, rotation, score]]
+            avg_linvel += metric_array[0]
+            current_goals = metric_array[2]
+
+            # Reset score on new game
+            if self.previous_goal_count > 0 and current_goals == 0:
+                self.previous_goal_count = 0
+            # Only count new goals
+            if current_goals > self.previous_goal_count:
+                self.previous_goal_count = current_goals
+        
         avg_linvel /= len(collected_metrics)
         report = {"x_vel":avg_linvel[0],
                   "y_vel":avg_linvel[1],
                   "z_vel":avg_linvel[2],
-                  "Cumulative Timesteps":cumulative_timesteps}
+                  "Cumulative Timesteps":cumulative_timesteps,
+                  "Goals_scored": self.previous_goal_count}
         wandb_run.log(report)
