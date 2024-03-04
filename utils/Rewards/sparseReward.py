398800b73e78c4597cd401cd2e69115c1d26a62b
Joeyjdl <joeylaarhoven@gmail.com>
2024-03-04 22:52:51 +0100

    added rocketleagueReward

--- utils/Rewards/sparseReward.py
+++ utils/Rewards/sparseReward.py
@@ -1,4 +1,8 @@
 from rlgym_sim.utils.reward_functions.common_rewards import  EventReward
 
-def spareReward():
+def sparseReward():
     return EventReward(goal=100, concede=-100, shot=10, save=50)
+
+# todo add all rewards
+def rocketLeagueReward():
+    return EventReward(goal=100, concede=-100, shot=10, save=50, touch=2, demo=3)
