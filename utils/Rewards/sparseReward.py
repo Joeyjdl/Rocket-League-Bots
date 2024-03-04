133720263f739ab0fe016de5d27734ed5a9b07a9
NHClaessens <nhclaessens@gmail.com>
2024-03-04 23:00:06 +0100

    sparse

--- utils/Rewards/sparseReward.py
+++ utils/Rewards/sparseReward.py
@@ -1,4 +1,4 @@
 from rlgym_sim.utils.reward_functions.common_rewards import  EventReward
 
-def rocketReward():
+def sparseReward():
     return EventReward(goal=100, concede=-100, shot=10, save=50)
