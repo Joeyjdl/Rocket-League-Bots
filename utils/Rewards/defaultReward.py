68436792a0c77b37bd26bc0b949ea4b571109ef2
Joeyjdl <joeylaarhoven@gmail.com>
2024-03-13 22:38:15 +0100

    Added default reward

---
+++ utils/Rewards/defaultReward.py
@@ -0,0 +1,11 @@
+from rlgym_sim.utils.reward_functions import CombinedReward
+from rlgym_sim.utils.reward_functions.common_rewards import VelocityPlayerToBallReward, VelocityBallToGoalReward, EventReward
+
+def defaultReward():
+    rewards_to_combine = (VelocityPlayerToBallReward(),
+                            VelocityBallToGoalReward(),
+                            EventReward(team_goal=1, concede=-1, demo=0.1, touch=0.2))
+    reward_weights = (0.01, 0.1, 10.0)
+
+    return CombinedReward(reward_functions=rewards_to_combine,
+                            reward_weights=reward_weights)
